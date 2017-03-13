110845ce8095de07b98ef7632cba1e04a921c7aa
Dimitar Sestrimski <2286354s@student.gla.ac.uk>
2017-03-13 14:20:38 +0000

    added base.html and user registration

--- ryze/models.py
+++ ryze/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    # Remember if you use Python 2.7.x, define __unicode__ too!
+    def __str__(self):
+        return self.user.username
